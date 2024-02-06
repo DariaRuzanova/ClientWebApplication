0de78f3d84209e845c9bace2061da33b0cd8d4e5
Daria Ruzanova <dashochek-28@mail.ru>
2024-02-06 16:26:56 +0400

    добавлены описания к классу ClientEntity

--- Client.Repositoryes/ClientEntity.cs
+++ Client.Repositoryes/ClientEntity.cs
@@ -1,11 +1,18 @@
 using Client.Contracts;
 
 namespace Client.Repositories;
-
+/// <summary>
+/// Клиенты репозитория.
+/// </summary>
 public class ClientEntity
 {
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
     public int id { get; set; }
+    
+    /// <summary>
+    /// Имя клиента.
+    /// </summary>
     public string Name { get; set; }
-    
-    
 }
